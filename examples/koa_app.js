4ec4efcfa07d7010103c2a66da5b1df00a4d5632
Marvin Witt <nurmarvin@protonmail.com>
2020-12-12 18:17:13 +0100

    docs: add koa example app

---
+++ examples/koa_app.js
@@ -0,0 +1,23 @@
+const Koa = require('koa');
+const bodyParser = require('koa-bodyparser');
+const { InteractionType, InteractionResponseType, verifyKeyKoaMiddleware } = require('../dist');
+
+const app = new Koa();
+
+app.use(bodyParser());
+
+app.use('/interactions', verifyKeyKoaMiddleware(process.env.CLIENT_PUBLIC_KEY), (ctx) => {
+  const interaction = ctx.request.body;
+  if (interaction.type === InteractionType.COMMAND) {
+    res.send({
+      type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
+      data: {
+        content: 'Hello world',
+      },
+    });
+  }
+});
+
+app.listen(8999, () => {
+  console.log('Example app listening at http://localhost:8999');
+});
