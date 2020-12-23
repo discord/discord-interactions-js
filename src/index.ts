c3965c39a8ddc584a9f42e39599a4f19b885aeb8
Simon <770632+Ponjimon@users.noreply.github.com>
2020-12-23 16:11:07 +0100

    fix: prevent stalling requests (#10)

--- src/index.ts
+++ src/index.ts
@@ -155,7 +155,9 @@ function verifyKeyMiddleware(clientPublicKey: string): (req: Request, res: Respo
     const signature = (req.header('X-Signature-Ed25519') || '') as string;
 
     async function onBodyComplete(rawBody: Buffer) {
-      if (!(await verifyKey(rawBody, signature, timestamp, clientPublicKey))) {
+      try {
+        verifyKey(rawBody, signature, timestamp, clientPublicKey);
+      } catch (e) {
         res.statusCode = 401;
         res.end('Invalid signature');
         return;
