87b18ff70d003ff2f6773f79cea2114c8e234e3f
BrightSkyz <BrightSkyz@users.noreply.github.com>
2021-01-11 14:16:17 -0500

    Switch from using callbacks

--- src/__tests__/verifyKeyMiddleware.ts
+++ src/__tests__/verifyKeyMiddleware.ts
@@ -1,6 +1,5 @@
-import express, { Request, Response } from 'express';
+import { Request, Response } from 'express';
 import * as http from 'http';
-import DoneCallback = jest.DoneCallback;
 import { InteractionResponseType, InteractionType, verifyKeyMiddleware } from '../index';
 import { AddressInfo } from 'net';
 import {
@@ -11,7 +10,7 @@ import {
   signRequestWithKeyPair,
 } from './utils/SharedTestUtils';
 
-const expressApp = express();
+const expressApp = require('express')();
 
 const exampleApplicationCommandResponse = {
   type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
@@ -30,18 +29,17 @@ expressApp.post('/interactions', verifyKeyMiddleware(Buffer.from(validKeyPair.pu
 let expressAppServer: http.Server;
 let exampleInteractionsUrl: string;
 
-beforeAll(async (done: DoneCallback) => {
+beforeAll(async () => {
   await new Promise<void>((resolve) => {
     expressAppServer = expressApp.listen(0);
     resolve();
   });
   exampleInteractionsUrl = 'http://localhost:' + (expressAppServer.address() as AddressInfo).port + '/interactions';
-  done();
 });
 
 describe('verify key middleware', () => {
 
-  it('valid ping', async (done: DoneCallback) => {
+  it('valid ping', async () => {
     // Sign and verify a valid ping request
     const signedRequest = signRequestWithKeyPair(pingRequestBody, validKeyPair.secretKey);
     const exampleRequestResponse = await sendExampleRequest(exampleInteractionsUrl, {
@@ -50,10 +48,9 @@ describe('verify key middleware', () => {
       'content-type': 'application/json'
     }, signedRequest.body);
     expect(exampleRequestResponse.status).toBe(200);
-    done();
   });
 
-  it('valid application command', async (done: DoneCallback) => {
+  it('valid application command', async () => {
     // Sign and verify a valid application command request
     const signedRequest = signRequestWithKeyPair(applicationCommandRequestBody, validKeyPair.secretKey);
     const exampleRequestResponse = await sendExampleRequest(exampleInteractionsUrl, {
@@ -63,10 +60,9 @@ describe('verify key middleware', () => {
     }, signedRequest.body);
     const exampleRequestResponseBody = JSON.parse(exampleRequestResponse.body);
     expect(exampleRequestResponseBody).toStrictEqual(exampleApplicationCommandResponse);
-    done();
   });
 
-  it('invalid key', async (done: DoneCallback) => {
+  it('invalid key', async () => {
     // Sign a request with a different private key and verify with the valid public key
     const signedRequest = signRequestWithKeyPair(pingRequestBody, invalidKeyPair.secretKey);
     const exampleRequestResponse = await sendExampleRequest(exampleInteractionsUrl, {
@@ -75,10 +71,9 @@ describe('verify key middleware', () => {
       'content-type': 'application/json'
     }, signedRequest.body);
     expect(exampleRequestResponse.status).toBe(401);
-    done();
   });
 
-  it('invalid body', async (done: DoneCallback) => {
+  it('invalid body', async () => {
     // Sign a valid request and verify with an invalid body
     const signedRequest = signRequestWithKeyPair(pingRequestBody, validKeyPair.secretKey);
     const exampleRequestResponse = await sendExampleRequest(exampleInteractionsUrl, {
@@ -87,10 +82,9 @@ describe('verify key middleware', () => {
       'content-type': 'application/json'
     }, 'example invalid body');
     expect(exampleRequestResponse.status).toBe(401);
-    done();
   });
 
-  it('invalid signature', async (done: DoneCallback) => {
+  it('invalid signature', async () => {
     // Sign a valid request and verify with an invalid signature
     const signedRequest = signRequestWithKeyPair(pingRequestBody, validKeyPair.secretKey);
     const exampleRequestResponse = await sendExampleRequest(exampleInteractionsUrl, {
@@ -99,10 +93,9 @@ describe('verify key middleware', () => {
       'content-type': 'application/json'
     }, signedRequest.body);
     expect(exampleRequestResponse.status).toBe(401);
-    done();
   });
 
-  it('invalid timestamp', async (done: DoneCallback) => {
+  it('invalid timestamp', async () => {
     // Sign a valid request and verify with an invalid timestamp
     const signedRequest = signRequestWithKeyPair(pingRequestBody, validKeyPair.secretKey);
     const exampleRequestResponse = await sendExampleRequest(exampleInteractionsUrl, {
@@ -111,16 +104,14 @@ describe('verify key middleware', () => {
       'content-type': 'application/json'
     }, signedRequest.body);
     expect(exampleRequestResponse.status).toBe(401);
-    done();
   });
 
-  it('missing headers', async (done: DoneCallback) => {
+  it('missing headers', async () => {
     // Sign a valid request and verify with an invalid timestamp
     const exampleRequestResponse = await sendExampleRequest(exampleInteractionsUrl, {
       'content-type': 'application/json'
     }, exampleInteractionsUrl);
     expect(exampleRequestResponse.status).toBe(401);
-    done();
   });
 });
 
