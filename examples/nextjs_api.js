4ecf9565ecc445a407b274d22cc369fdb7443386
Taimoor Tariq <45585081+Taimoor-Tariq@users.noreply.github.com>
2022-03-13 19:24:37 -0400

    Added Next.JS API example

---
+++ examples/nextjs_api.js
@@ -0,0 +1,28 @@
+// ./pages/api/interaction.js
+
+const { InteractionType, InteractionResponseType, verifyKeyMiddleware } = require('../dist');
+
+function runMiddleware(req, res, fn) {
+  return new Promise((resolve, reject) => {
+    req.header = (name) => { return req.headers[name.toLowerCase()] };
+    req.body = JSON.stringify(req.body);
+    fn(req, res, (result) => {
+      if (result instanceof Error) return reject(result);
+      return resolve(result);
+    });
+  });
+}
+
+export default async function handler(req, res) {
+  await runMiddleware(req, res, verifyKeyMiddleware(process.env.CLIENT_PUBLIC_KEY));
+
+  const interaction = req.body;
+  if (interaction.type === InteractionType.APPLICATION_COMMAND) {
+    res.send({
+      type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
+      data: {
+        content: 'Hello world',
+      },
+    });
+  }
+}
