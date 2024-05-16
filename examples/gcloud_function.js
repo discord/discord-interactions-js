ccfcba9d5698ba0cf8e0902f9aac6a5ca8f16371
Justin Beckwith <justin.beckwith@discordapp.com>
2024-05-16 16:05:21 -0700

    chore: use biome for formatting and linting

--- examples/gcloud_function.js
+++ examples/gcloud_function.js
@@ -1,28 +1,37 @@
-const { InteractionResponseType, InteractionType, verifyKey } = require('discord-interactions');
+const {
+	InteractionResponseType,
+	InteractionType,
+	verifyKey,
+} = require('discord-interactions');
 
 const CLIENT_PUBLIC_KEY = process.env.CLIENT_PUBLIC_KEY;
 
 module.exports.myInteraction = async (req, res) => {
-  // Verify the request
-  const signature = req.get('X-Signature-Ed25519');
-  const timestamp = req.get('X-Signature-Timestamp');
-  const isValidRequest = await verifyKey(req.rawBody, signature, timestamp, CLIENT_PUBLIC_KEY);
-  if (!isValidRequest) {
-    return res.status(401).end('Bad request signature');
-  }
+	// Verify the request
+	const signature = req.get('X-Signature-Ed25519');
+	const timestamp = req.get('X-Signature-Timestamp');
+	const isValidRequest = await verifyKey(
+		req.rawBody,
+		signature,
+		timestamp,
+		CLIENT_PUBLIC_KEY,
+	);
+	if (!isValidRequest) {
+		return res.status(401).end('Bad request signature');
+	}
 
-  // Handle the payload
-  const interaction = req.body;
-  if (interaction && interaction.type === InteractionType.APPLICATION_COMMAND) {
-    res.send({
-      type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
-      data: {
-        content: `You used: ${interaction.data.name}`,
-      },
-    });
-  } else {
-    res.send({
-      type: InteractionResponseType.PONG,
-    });
-  }
+	// Handle the payload
+	const interaction = req.body;
+	if (interaction && interaction.type === InteractionType.APPLICATION_COMMAND) {
+		res.send({
+			type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
+			data: {
+				content: `You used: ${interaction.data.name}`,
+			},
+		});
+	} else {
+		res.send({
+			type: InteractionResponseType.PONG,
+		});
+	}
 };
